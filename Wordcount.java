37206efb7e4e2010d36a4f631c9e0b748b49e936
sonali350 <sonalimalviya736@gmail.com>
2024-09-10 22:43:53 +0530

    Add files via upload

---
+++ Wordcount.java
@@ -0,0 +1,23 @@
+
+
+ class Wordcount {
+    
+    public static void main(String[] args) {
+        
+        String str = "This is a string lan me";
+        int count=0;
+    
+        for(int i=0; i<str.length(); i++){
+
+                char ch= str.charAt(i);
+                if(ch==' '){
+                    count++;
+                }
+                
+            }
+                System.out.println(count+1); 
+            
+         
+      
+    }
+}
