21557d5b35ffcdbeac80c0bfa03c175a9fbe1fc5
sonali350 <sonalimalviya736@gmail.com>
2024-09-10 22:59:07 +0530

    Add files via upload

---
+++ ConcateString.java
@@ -0,0 +1,9 @@
+public class ConcateString {
+    public static void main(String[] args) {
+        String s1 = "Php Exercises";
+        String s2 = "Python Exercises";
+        String FinalString = s1+s2;
+        System.out.println(FinalString);
+    }
+    
+}
