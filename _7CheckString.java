3d9000e5d6aeaab7d31be592385a8055367dedfe
sonali350 <sonalimalviya736@gmail.com>
2024-09-10 23:01:43 +0530

    Add files via upload

---
+++ _7CheckString.java
@@ -0,0 +1,11 @@
+public class _7CheckString {
+    public static void main(String[] args) {
+        String str1 = new String("Stephen Edwin King" );
+        String str2 =new String ("Walter Winchell");
+        String str3 = new String("Walter Winchell");
+        boolean res = str2.equals(str3);
+      //  boolean res1 = st
+        System.out.println(res);
+       
+    }
+}
